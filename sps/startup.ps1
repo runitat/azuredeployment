93c157b99aed3edf75e6b0c9afc2f2c19ad0095c
Philipp Gigler <philipp.gigler@runit.at>
2017-12-21 10:32:58 +0100

    new startup script and default-file

--- sps/startup.ps1
+++ sps/startup.ps1
@@ -1,1 +1,23 @@
-New-Item -ItemType directory -Path C:\SPS
+# __________________________________________________
+#
+# Setup
+#
+
+# __________________________________________________
+#
+# Variables
+#
+$Computer = (Get-WmiObject Win32_ComputerSystem).Name
+$Domain = (Get-WmiObject Win32_ComputerSystem).Domain
+
+# __________________________________________________
+#
+# Add
+#
+function addDomUserToLocalGroup($user, $group) {
+	$reference = [ADSI]"WinNT://$Computer/$group"
+	$reference.Add("WinNT://$Domain/$user")
+}
+
+addDomUserToLocalGroup "Remote Desktop Users" "RDPUser"
+addDomUserToLocalGroup "Administrators" "manager"
