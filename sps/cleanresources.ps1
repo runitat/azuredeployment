0ddde6aa3a8a91a5706d67ded768ff57503fd393
Philipp Gigler <philipp.gigler@runit.at>
2018-06-15 11:20:00 +0200

    System-Clean-Script Update

--- sps/cleanresources.ps1
+++ sps/cleanresources.ps1
@@ -3,38 +3,41 @@
 # https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-windows?view=azure-cli-latest
 #
 
-Param(
-	[Boolean] $List = $false
-)
-
 # Login
+$User = "VALUE"
 Write-Output "___ Login ___"
-az login
+Write-Output "User: $($User)"
+az login -u $User
 Write-Output ""
 
 # Task
-$Interfaces = az network nic list --query '[?virtualMachine==`null`].[id]' -o tsv
 $Progress = 0
-
-foreach ($Interface in $Interfaces) {
-	$Progress++
-	Write-Output "___ Resource ($Progress/$($Interfaces.length)) ___"
-	if (!$List) {
-		Write-Output "Looking for Resource-Connections..."
-		$NIC = az network nic show --ids $Interface --query 'id'
-		$PIP = az network nic show --ids $Interface --query 'ipConfigurations[0].publicIpAddress.id'
-		$NSG = az network nic show --ids $Interface --query 'networkSecurityGroup.id'
-		Write-Output "Deleting Network-Interface: $($NIC)"
-		az network nic delete --ids $NIC
-		Write-Output "Deleting Public-IP-Adress: $($PIP)"
-		az network public-ip delete --ids $PIP
-		Write-Output "Deleting Network-Security-Group: $($NSG)"
-		az network nsg delete --ids $NSG
+Write-Output "Searching for unattached Network Interfaces..."
+$Interfaces = az network nic list --query '[?virtualMachine==`null`].[id]' -o tsv
+Write-Output "Found $($Interfaces.length) unattached Network Interfaces."
+Write-Output ""
+If($Interfaces.length -ne 0) {
+	$Action = Read-Host -Prompt "Do you want to delete all Network Interfaces? (y/n)"
+	foreach ($Interface in $Interfaces) {
+		$Progress++
+		Write-Output "___ Resource ($Progress/$($Interfaces.length)) ___"
+		if ($Action -eq "y") {
+			Write-Output "Looking for Resource-Connections..."
+			$NIC = az network nic show --ids $Interface --query 'id'
+			$PIP = az network nic show --ids $Interface --query 'ipConfigurations[0].publicIpAddress.id'
+			$NSG = az network nic show --ids $Interface --query 'networkSecurityGroup.id'
+			Write-Output "Deleting Network-Interface: $($NIC)"
+			az network nic delete --ids $NIC
+			Write-Output "Deleting Public-IP-Adress: $($PIP)"
+			az network public-ip delete --ids $PIP
+			Write-Output "Deleting Network-Security-Group: $($NSG)"
+			az network nsg delete --ids $NSG
+		}
+		else {
+			az network nic show --ids $Interface --query '{NIC: id, PIP: ipConfigurations[0].publicIpAddress.id, NSG: networkSecurityGroup.id}'
+		}
+		Write-Output ""
 	}
-	else {
-		az network nic show --ids $Interface --query '{NIC: id, PIP: ipConfigurations[0].publicIpAddress.id, NSG: networkSecurityGroup.id}'
-	}
-	Write-Output ""
 }
 
 # End
